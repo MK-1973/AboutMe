e3a7cd88725e58cbf200adb28d0f7952c14a00aa
MK-1973 <m.kosov-1973@yandex.ru>
2022-02-13 23:16:24 +0500

    Добавьте файлы проекта.

---
+++ AboutMe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AboutMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Mikhail";
+            byte myAge = 45;
+            bool iHavePet = true;
+            double myShoeSize = 43.5;
+
+            Console.WriteLine("My name is " + myName);
+            Console.WriteLine("My age is " + myAge);
+            Console.WriteLine("Do I have a pet? " + iHavePet);
+            Console.WriteLine("My shoe size is " + myShoeSize);
+        }
+    }
+}
